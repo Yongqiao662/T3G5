d48934f5010f94813588e58c7f23fc2dc8652d9e
Yongqiao662 <s2184898@siswa.um.edu.my>
2024-12-18 23:20:01 +0800

    Update TeachingStrategyApp.java

--- TeachingStrategyApp.java
+++ TeachingStrategyApp.java
@@ -1,15 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package teaching.strategy.prediction.recommender.website;
-import java.util.Scanner;
-
-
-/**
- *
- * @author Yong Qiao
- */
 
 
 import java.util.Scanner;
